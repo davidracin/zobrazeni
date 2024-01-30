f6abbbcd79de2704c220705b43a3ec99b90a47fb
davidracin <david.racin06@gmail.com>
2024-01-30 18:00:51 +0100

    deleted mistakes

--- src/SpravceDeskovek.java
+++ src/SpravceDeskovek.java
@@ -3,8 +3,6 @@ import java.util.List;
 
 public class SpravceDeskovek {
     private List<Deskovka> seznamDeskovek = new ArrayList<>();
-    Deskovka deskovka1 = new Deskovka("Carcassonne", true, 3);
-    seznamDeskovek.add(deskovka1);
 
     public Deskovka getDeskovka(int index) {
         return seznamDeskovek.get(index);
