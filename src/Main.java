a260eb73eb8a4cd0668731561f14c4c6ad94c6a7
davidracin <david.racin06@gmail.com>
2024-02-03 00:55:43 +0100

    Added reading from text file. Fixed array size is null. When you get to the end of list next and previous button is disabled.

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        SpravceDeskovek spravceDeskovek = new SpravceDeskovek();
-        GUI gui = new GUI(spravceDeskovek);
+        GUI gui = new GUI();
         gui.setVisible(true);
     }
 }
